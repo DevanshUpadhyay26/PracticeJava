2d5959d5d831dfe70c7ade5f08595dcac45511ce
DevanshUpadhyay26 <Devanshupadhyay26@gmail.com>
2024-10-02 17:42:47 +0530

    Completed OOPS and String Classes

---
+++ src/objectOriented/inheritanceTypes/inheritanceExamples.java
@@ -0,0 +1,22 @@
+package objectOriented.inheritanceTypes;
+// Single Inheritance
+class University{
+
+    void University(int _dept_id, int num_of_faculties){
+        System.out.println("Department_ID: " +_dept_id + " Number of Faculties: "+num_of_faculties);
+    }
+}
+class Departments extends University {
+    void display(){
+        super.University(1,100);
+    }
+
+
+}
+public class inheritanceExamples {
+    
+    public static void main(String[] args) {
+        Departments d = new Departments();
+        d.display();
+    }
+}
---
+++ src/patternQuestions/ptq.java
@@ -0,0 +1,2 @@
+package patternQuestions;public class ptq {
+}
